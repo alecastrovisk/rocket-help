09c7804c5592fc1149081e8fc48e4162398bf348
Alexandre Castro <alexandre.acrsf@gmail.com>
2022-07-20 10:13:38 -0300

    feat: Adding navigation

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -9,6 +9,8 @@ import {
   Center
 } from 'native-base';
 
+import { useNavigation } from '@react-navigation/native';
+
 import { SignOut, ChatTeardropText } from 'phosphor-react-native';
 import { useState } from 'react';
 import Logo from '../assets/logo_secondary.svg';
@@ -19,9 +21,27 @@ import { Order, OrderProps } from '../components/Order';
 
 export function Home() {
   const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
-  const [orders, setOrders] = useState<OrderProps[]>([]);
+  const [orders, setOrders] = useState<OrderProps[]>([
+    {    
+      id: '452',
+      patrimony: '123456',
+      status: 'open',
+      when: '18/07/2022'
+    }
+
+  ]);
 
   const { colors } = useTheme();
+
+  const navigation = useNavigation();
+
+  function handleNewOrder() {
+    navigation.navigate('new')
+  }
+
+  function handleOpenDetails(orderId: string) {
+    navigation.navigate('details', { orderId })
+  }
   return (
     <VStack flex={1} pb={6} bg="gray.700">
       <HStack
@@ -68,7 +88,11 @@ export function Home() {
         <FlatList
           data={orders}
           keyExtractor={item => item.id}
-          renderItem={({ item }) => <Order data={item} /> }
+          renderItem={({ item }) => (
+            <Order 
+              data={item} 
+              onPress={() => handleOpenDetails(item.id)} />
+          )}
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{ paddingBottom: 100 }}
           ListEmptyComponent={() => (
@@ -82,7 +106,10 @@ export function Home() {
           )}
         />
 
-        <Button title='Nova solicitação'/>
+        <Button
+         title='Nova solicitação'
+         onPress={handleNewOrder}
+        />
       </VStack>
 
     </VStack>
